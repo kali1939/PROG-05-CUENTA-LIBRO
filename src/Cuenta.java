0e597924ed8aebdc7a8d6d1dd8c2f8d7cd5f53c3
Sergio <smartines303@gmail.com>
2022-05-01 21:29:18 -0500

    Primera version

---
+++ src/Cuenta.java
@@ -0,0 +1,13 @@
+
+public class Cuenta {
+
+    private String nombre;
+    public void establecerNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String obtenernombre()     
+    {
+        return nombre;
+    }
+}
